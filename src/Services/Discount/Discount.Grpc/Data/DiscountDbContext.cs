9efd1546285e24a7e568ccfada67f80563e4d5bf
abdelelrahmanali.mohamed <abdelrahmanali.mohamed@superhog.com>
2025-03-26 16:05:40 +0100

    implementing gRPC

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+	public class DiscountDbContext : DbContext
+	{
+		public DbSet<Coupon> Coupons { get; set; }
+		public DiscountDbContext(DbContextOptions<DiscountDbContext> options) 
+			: base(options)
+		{
+			
+		}
+	}
+}
---
+++ src/Services/Discount/Discount.Grpc/Data/Extension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+public static class Extension
+{
+	public static IApplicationBuilder UseMigration(this IApplicationBuilder builder)
+	{
+		using var scope = builder.ApplicationServices.CreateScope();
+		using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountDbContext>();
+		dbContext.Database.MigrateAsync();
+		return builder;
+	}
+}
