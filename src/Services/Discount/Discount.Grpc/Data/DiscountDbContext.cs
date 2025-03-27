4704efde1f2e4aba1f8a269aecf043fbb5b946dc
abdelelrahmanali.mohamed <abdelrahmanali.mohamed@superhog.com>
2025-03-27 14:37:39 +0100

    implementing CRUD operation for discount grpc and add it to docker compose file

--- src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -1,15 +1,19 @@
 ﻿using Discount.Grpc.Model;
 using Microsoft.EntityFrameworkCore;
 
-namespace Discount.Grpc.Data
+namespace Discount.Grpc.Data;
+public class DiscountDbContext : DbContext
 {
-	public class DiscountDbContext : DbContext
+	public DiscountDbContext(DbContextOptions<DiscountDbContext> options) 
+		: base(options)
 	{
-		public DbSet<Coupon> Coupons { get; set; }
-		public DiscountDbContext(DbContextOptions<DiscountDbContext> options) 
-			: base(options)
-		{
-			
-		}
 	}
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Coupon>().HasData(
+			new Coupon {Id = 1 , ProductName = "IPhone X" , Description = "" , Amount = 10} ,
+			new Coupon { Id = 2 , ProductName = "Samsung x" , Description = "" , Amount = 5}
+			);
+	}
+	public DbSet<Coupon> Coupons { get; set; }
 }
--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,29 +1,63 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Model;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 namespace Discount.Grpc.Services;
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountDbContext dbContext, ILogger<DiscountService> logger)
+	: DiscountProtoService.DiscountProtoServiceBase
 {
-	public override Task<CouponModel> CreateDiscount
-		(CreateDiscountRequest request, 
+	public override async Task<CouponModel> CreateDiscount
+		(CreateDiscountRequest request,
 		ServerCallContext context)
 	{
-		return base.CreateDiscount(request, context);
+		var coupon = request.Coupon.Adapt<Coupon>();
+		if (coupon is null)
+			throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request"));
+
+		dbContext.Coupons.Add(coupon);
+		await dbContext.SaveChangesAsync();
+
+		return coupon.Adapt<CouponModel>();
 	}
-	public override Task<DeleteDiscountResponse> DeleteDiscount
-		(DeleteDiscountRequest request, 
+	public override async Task<DeleteDiscountResponse> DeleteDiscount
+		(DeleteDiscountRequest request,
 		ServerCallContext context)
 	{
-		return base.DeleteDiscount(request, context);
+		var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+		if (coupon is null)
+			throw new RpcException(new Status(StatusCode.NotFound, $"Discount for {request.ProductName} is not found"));
+
+		dbContext.Coupons.Remove(coupon);
+		await dbContext.SaveChangesAsync();
+
+		return new DeleteDiscountResponse { Sucess = true };
 	}
-	public override Task<CouponModel> GetDiscount
-		(GetDiscountRequest request, 
+	public override async Task<CouponModel> GetDiscount
+		(GetDiscountRequest request,
 		ServerCallContext context)
 	{
-		return base.GetDiscount(request, context);
+		var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+		if (coupon is null)
+			coupon = new Coupon { ProductName = "No Product Found" };
+
+		logger.LogInformation("Dicount is retrieved for Product Name {productName}", coupon.ProductName);
+
+		return coupon.Adapt<CouponModel>();
 	}
-	public override Task<CouponModel> UpdateDiscount
-		(UpdateDiscountRequest request, 
+	public override async Task<CouponModel> UpdateDiscount
+		(UpdateDiscountRequest request,
 		ServerCallContext context)
 	{
-		return base.UpdateDiscount(request, context);
+		var coupon = request.Coupon.Adapt<Coupon>();
+		if (coupon is null)
+			throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request"));
+
+		dbContext.Coupons.Update(coupon);
+		await dbContext.SaveChangesAsync();
+
+		return coupon.Adapt<CouponModel>();
 	}
 }
